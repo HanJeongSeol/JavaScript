221c7513435b856cf076b674a77f5f8b6698a34a
hanjeong <hanjeong94@naver.com>
2019-08-28 17:47:53 +0900

    08_28

--- chapter4.js
+++ chapter4.js
@@ -9,31 +9,32 @@
 // console.log(typeof(undefined_check));
 // console.log(typeof(null_check));
 
-let symbol1 = Symbol('key');
-let symbol2=  Symbol('key');
-let person = {
-    name: "hanjeong",
-    gender: "male",
-    age: 26,
-    isCool: true,
-    favSong: ["비", "너를만나", "Not Over Yet", "이별", "휴가"],
-    favFood: [
-        {
-            name: "burger",
-            rice: false
-        },
-        {
-            name: "coffee",
-            rice: false
-        }
-    ]
-}
-person[symbol1] = 'value1';
-person[symbol2] = 'value2';
-console.log(person);
-console.log(person[symbol1] === 'value1');
-console.log(person[symbol2] === 'value2');
-console.log(symbol1 !== symbol2);
+// let symbol1 = Symbol('key');
+// let symbol2=  Symbol('key');
+// let person = {
+//     name: "hanjeong",
+//     gender: "male",
+//     age: 26,
+//     isCool: true,
+//     favSong: ["비", "너를만나", "Not Over Yet", "이별", "휴가"],
+//     favFood: [
+//         {
+//             name: "burger",
+//             rice: false
+//         },
+//         {
+//             name: "coffee",
+//             rice: false
+//         }
+//     ]
+// }
+// person[symbol1] = 'value1';
+// person[symbol2] = 'value2';
+// console.log(person);
+// console.log(person[symbol1] === 'value1');
+// console.log(person[symbol2] === 'value2');
+// console.log(symbol1 !== symbol2);
+// console.log(typeof symbol1 === 'symbol');
 // console.log(person);
 // console.log(person.favSong[0]);
 // console.log(person.favFood[1].rice);
@@ -41,3 +42,16 @@ console.log(symbol1 !== symbol2);
 // person.age = 20;
 // console.log(person.age);
 // console.log(Math.pow(2, 2));
+
+let mon = 'monday';
+let tus = 'tusday';
+let wed = 'wednesday';
+let day = ['monday','tuesday','wednesday','thursday','friday','saturday','sunday']
+document.write("today is "+mon+"<br>");
+document.write("today is "+day[0]);
+let arr = new Array('string', 1, true);
+console.log(typeof arr[0]);     //string
+console.log(typeof arr[1]);     //number
+console.log(typeof arr[2]);     //boolen
+console.log(arr.length);
+console.log(typeof arr);
---
+++ chapter5.js
@@ -0,0 +1,49 @@
+// let x = 12;
+// let y = 5;
+// console.log(x+y);   // 17
+// console.log(x-y);   // 7
+// console.log(x*y);   // 60
+// console.log(x/y);   // 2.4
+// console.log(x%y);   // 2
+
+// console.log(x+=5);  // 17
+// console.log(x-=5);  // 12
+// console.log(x*=5);  // 60
+// console.log(x/=5);  // 12
+// console.log(x%=5);  // 2
+// let a = 3;
+// let b = 3;
+// console.log((++a) -1);  // 3
+// console.log(a)  // 4
+// console.log((b++) -1);  // 2
+// console.log(b); // 4 
+// a =3;
+// b =3;
+// console.log((--a) +1);  //3
+// console.log(a); // 2
+// console.log((b--) +1);  // 4
+// console.log(b); // 2
+
+// let a = 11;
+// console.log(++a + 3 - a++ - --a + a--); // 3
+// console.log(a)  //11
+
+// console.log(2=='2');    // true 
+// console.log(2==='2');   // false
+// console.log(3!='3');    // false
+// console.log(3!=='3');   // true
+// console.log(2>3);   // false
+// console.log(3>=3);  // true
+// console.log(2<3);   // true
+// console.log(2<=3);  // true
+
+// let x = true; 
+// let y = false;
+
+// console.log(x && y);    // false
+// console.log(x || y);    // true
+// console.log(!x);    // flase
+// console.log(!y);    // true
+// console.log(2<<2);
+// console.log(8>>2);
+// console.log(2>>>1);
