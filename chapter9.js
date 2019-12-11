bd7a383bc1cd0703c222505bddf99e537ebcbe34
hanjeong <hanjeong94@naver.com>
2019-12-11 12:41:48 +0900

    1211

---
+++ chapter9.js
@@ -0,0 +1,36 @@
+// var x = 'xxx';
+// function foo () {
+//   var y = 'yyy';
+
+//   function bar () {
+//     var z = 'zzz';
+//     console.log(x + y + z);
+//   }
+//   bar();
+// }
+// foo();
+
+// console.dir(foo);
+// console.dir(window);
+
+// let x = 'xxx';
+// function foo () {
+//     let y = 'yyy';
+
+//   function bar () {
+//     let z = 'zzz';
+//     console.log(x + y + z);
+//   }
+//   bar();
+//   console.dir(bar);
+//   console.log(bar);
+// }
+// foo();
+
+// console.dir(foo);
+
+function first(a,b){
+	console.log(a+b);
+}
+first(3,4); 
+console.dir(window);
