6a2241759e0c8fc00694fd43acb357fe13a76a08
hanjeong <hanjeong94@naver.com>
2019-12-13 16:29:55 +0900

    1213

--- chapter9.js
+++ chapter9.js
@@ -1,17 +1,20 @@
-// var x = 'xxx';
-// function foo () {
-//   var y = 'yyy';
+var x = 'xxx';
+function foo () {
+  var y = 'yyy';
+  var c = 'ccc';
+  function bar () {
+    var z = 'zzz';
+    console.log(x + y + z);
+  }
+  console.dir(bar);
 
-//   function bar () {
-//     var z = 'zzz';
-//     console.log(x + y + z);
-//   }
-//   bar();
-// }
-// foo();
+  bar();
+}
+foo();
+console.log(c);
 
-// console.dir(foo);
-// console.dir(window);
+console.dir(foo);
+console.dir(window);
 
 // let x = 'xxx';
 // function foo () {
@@ -29,8 +32,112 @@
 
 // console.dir(foo);
 
-function first(a,b){
-	console.log(a+b);
-}
-first(3,4); 
-console.dir(window);
+// function first(a,b){
+// 	console.log(a+b);
+// }
+// first(3,4); 
+// console.dir(window);
+
+
+// // 렉시컬 환경
+// function printName() {
+//     return 'hanjeong'
+//   }
+  
+//   function findName() {
+//     return printName()
+//   }
+  
+//   function sayMyName() {
+//     return findName()
+//   }
+
+//   function print(){
+//       function printName(){
+//           return 'Hyunso';
+//       }
+//   }
+  
+//   console.log(sayMyName());
+
+// // 함수 호이스팅_1
+
+// console.log(foo);       // undefined
+// console.log(sayName());     // hanjeong
+
+// var foo = 'hanjeong';
+
+// function sayName(){
+//     return 'hanjeong';
+// }
+
+// // 함수 호이스팅_2
+// var foo = undefined;
+// function sayName(){
+//     return 'hanjeong';
+// }
+
+// console.log(foo);       // undefined
+// console.log(sayName());     // hanjeong
+
+// var foo = 'hanjeong';
+
+// // 함수 호이스팅_3
+
+// console.log(foo);       // ReferenceError
+// console.log(sayName());     // hanjeong
+
+// let foo = 'hanjeong';
+
+// function sayName(){
+//     return 'hanjeong';
+// }
+
+
+// // 함수 호이스팅_4
+// console.log(sayName2);  // undefined
+// console.log(sayName2());    // Uncaught TypeError: sayName2 is not a function
+// console.log(sayName()); // hanjeong
+
+// // 함수 표현식
+// var sayName2 = function(){
+//     return 'hanjeong';
+// };
+// // 함수 선언식
+// function sayName(){
+//     return 'hanjeong';
+// }
+
+
+// // 함수 호이스팅_5
+// var favouriteFood = 'grapes';
+
+// var foodThoughts = function () {
+//   console.log("Original favourite food: " + favouriteFood); // undefined
+  
+//   var favouriteFood = 'sushi';
+//   console.log("New favourite food: " + favouriteFood); // sushi
+// };
+
+// foodThoughts();
+
+// // 함수 호이스팅_5의 컨텍스트
+
+// // global 실행 컨텍스트
+// var favouriteFood = undefined; // hoisting
+// var foodThoughts = undefined; // hoisting
+
+// favouriteFood = 'grapes';
+
+//       // foodThought 실행 컨텍스트
+//       foodThoughts = function () {
+//         var favouriteFood = undefined; // hoisting
+//         console.log("Original favourite food: " + favouriteFood);
+
+//         favouriteFood = 'sushi';
+//         console.log("New favourite food: " + favouriteFood);
+//       };
+//       // foodThought 실행 컨텍스트
+// // global 실행 컨텍스트
+
+// foodThoughts();
